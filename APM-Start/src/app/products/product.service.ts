7499375aa6c5b5296f8e95ba63a4b415802edb97
Rocio Garrido <rocio.garrido@wefox.com>
2022-10-20 10:05:50 +0200

    Map array elements

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -13,7 +13,8 @@ export class ProductService {
   products$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
       // map(item => item.price * 1.5),
-      map(products => products),
+      map(products =>
+        products.map(product => product.price ? product.price * 1.5 : 0)),
       tap(data => console.log('Products: ', JSON.stringify(data))),
       catchError(this.handleError)
     );
