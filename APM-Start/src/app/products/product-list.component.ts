f707147e40d6b1ce1c08e8e564cbd500fb21041b
Rocio Garrido <rocio.garrido@wefox.com>
2022-10-24 13:16:29 +0200

    Filtering emitted items

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { catchError, EMPTY } from 'rxjs';
+import { catchError, EMPTY, map } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
 import { ProductService } from './product.service';
 
@@ -12,6 +12,7 @@ export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
+  selectedCategoryId = 1;
 
   products$ = this.productService.productsWithCategory$
     .pipe(
@@ -20,6 +21,14 @@ export class ProductListComponent {
         return EMPTY;
       })
     );
+
+  productsSimpleFilter$ = this.productService.productsWithCategory$
+    .pipe(
+      map(products =>
+        products.filter(product =>
+          this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true
+        ))
+    )
 
   constructor(private productService: ProductService) { }
 
