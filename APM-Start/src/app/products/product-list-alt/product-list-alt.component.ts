da7d93753f4f18fd433eb1befdfbb863dc541772
Rocio Garrido <rocio.garrido@wefox.com>
2022-10-31 09:08:15 +0100

    Reacting to a selection: action stream

--- APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { catchError, EMPTY, Subscription } from 'rxjs';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
@@ -7,12 +7,12 @@ import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-list',
-  templateUrl: './product-list-alt.component.html'
+  templateUrl: './product-list-alt.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductListAltComponent {
   pageTitle = 'Products';
   errorMessage = '';
-  selectedProductId = 0;
 
   products$ = this.productService.productsWithCategory$
     .pipe(
@@ -25,9 +25,11 @@ export class ProductListAltComponent {
   products: Product[] = [];
   sub!: Subscription;
 
+  selectedProduct$ = this.productService.selectedProduct$;
+
   constructor(private productService: ProductService) { }
 
   onSelected(productId: number): void {
-    console.log('Not yet implemented');
+    this.productService.selectedProductChanged(productId);
   }
 }
--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, combineLatest, map, Observable, tap, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, combineLatest, map, Observable, tap, throwError } from 'rxjs';
 import { ProductCategoryService } from '../product-categories/product-category.service';
 import { Product } from './product';
 
@@ -31,16 +31,25 @@ export class ProductService {
     )
   );
 
-  selectedProduct$ = this.productsWithCategory$
-    .pipe(
-      map(products =>
-        products.find(product => product.id === 5)
-      ),
-      tap(product => console.log('selectedProduct', product))
-    );
+  private productSelectedSubject = new BehaviorSubject<number>(0);
+  productSelectedAction$ = this.productSelectedSubject.asObservable();
+
+  selectedProduct$ = combineLatest([
+    this.productsWithCategory$,
+    this.productSelectedAction$
+  ]).pipe(
+    map(([products, selectedProductId]) =>
+      products.find(product => product.id === selectedProductId)
+    ),
+    tap(product => console.log('selectedProduct', product)),
+  )
 
   constructor(private http: HttpClient,
     private productCategoryService: ProductCategoryService) { }
+
+  selectedProductChanged(selectedProductId: number): void {
+    this.productSelectedSubject.next(selectedProductId);
+  }
 
   private fakeProduct(): Product {
     return {
