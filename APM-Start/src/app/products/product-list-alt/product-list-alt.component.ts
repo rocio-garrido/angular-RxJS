10a2af86fb5733b067427d8eb2c9af1869f6a615
Rocio Garrido <rocio.garrido@wefox.com>
2022-10-31 09:15:52 +0100

    Reacting to an error

--- APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { catchError, EMPTY, Subscription } from 'rxjs';
+import { catchError, EMPTY, Subject, Subscription } from 'rxjs';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 
@@ -12,12 +12,13 @@ import { ProductService } from '../product.service';
 })
 export class ProductListAltComponent {
   pageTitle = 'Products';
-  errorMessage = '';
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
 
   products$ = this.productService.productsWithCategory$
     .pipe(
       catchError(err => {
-        this.errorMessage = err;
+        this.errorMessageSubject.next(err);
         return EMPTY;
       })
     );
